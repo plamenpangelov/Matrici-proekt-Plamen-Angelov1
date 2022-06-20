578adc55d374132f612e5cf94a4e7accab6b08c0
plamenpangelov <plamen.p.angelov1@gmail.com>
2022-06-20 19:19:55 +0300

    20-06-22

--- src/Main.java
+++ src/Main.java
@@ -17,24 +17,20 @@ import java.util.Scanner;
 
 public class Main {
     // Function to print Matrix
-    static void print_matrix(int output[][], int r, int c)
-    {
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
+    static void print_matrix(int output[][], int r, int c) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 System.out.print(output[i][j] + " ");
             }
             System.out.println();
         }
     }
+
     // Function to multiply two matrices a[][] and b[][]
-    static void multiply(int r1, int c1, int r2, int c2, int a[][], int b[][])
-    {
+    static void multiply(int r1, int c1, int r2, int c2, int a[][], int b[][]) {
         int i, j, k;
         // Check if multiplication is Possible
-        if(r2!=c1)
-        {
+        if (r2 != c1) {
             System.out.println("\n We can’t multiply these matrices.");
             return;
         }
@@ -42,17 +38,14 @@ public class Main {
         //The product matrix will be of size row1 x col2
         int c[][] = new int[r1][c2];
         // Multiply the two marices
-        for(i=0;i<r1;i++)
-        {
-            for(j=0;j<c2;j++)
-            {
-                for(k=0;k<r2;k++)
-                {
-                    c[i][j]+=a[i][k]*b[k][j];
+        for (i = 0; i < r1; i++) {
+            for (j = 0; j < c2; j++) {
+                for (k = 0; k < r2; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
                 }
             }
         }
-        print_matrix(c,r1,c2);
+        print_matrix(c, r1, c2);
     }
 
     public static void main(String[] args) {
@@ -93,26 +86,25 @@ public class Main {
             System.out.println("\n We can't add these matrices.");
         } else {
             int c[][] = new int[n][m];
-            for (int i = 0; i <  matrix1.length; i++) {
-                for (int j = 0; j <  matrix2[0].length; j++) {
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix2[0].length; j++) {
                     c[i][j] = matrix1[i][j] + matrix2[i][j];
                     System.out.print(c[i][j] + "|");
                 }
                 System.out.println();
-
+            }
+            System.out.println();
             int d[][] = new int[n][m];
-            for (i = 0; i < matrix1.length; i++) {
-                for (int j = 0; j< matrix2[0].length; j++) {
+            for (int i = 0; i < matrix1.length; i++) {
+                for (int j = 0; j < matrix2[0].length; j++) {
                     d[i][j] = matrix1[i][j] - matrix2[i][j];
                     System.out.print(d[i][j] + "|");
-                    }
-                    System.out.println();
-
                 }
+                System.out.println();
             }
         }
 
-        int r1,c1,r2,c2;
+        int r1, c1, r2, c2;
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter the number of row in multiplication matrix a:  ");
         r1 = inp.nextInt();
@@ -123,24 +115,20 @@ public class Main {
         System.out.print("Enter the number of column in multiplication matrix b:  ");
         c2 = inp.nextInt();
         int a[][] = new int[r1][c1];
-        for(int i=0;i<r1;i++)
-        {
-            for(int j=0;j<c1;j++)
-            {
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
                 System.out.printf("a[%d,%d] = ", i, j);
-                a[i][j]=inp.nextInt();
+                a[i][j] = inp.nextInt();
             }
         }
         int b[][] = new int[r2][c2];
-        for(int i=0;i<r2;i++)
-        {
-            for(int j=0;j<c2;j++)
-            {
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
                 System.out.printf("b[%d,%d] = ", i, j);
-                b[i][j]=inp.nextInt();
+                b[i][j] = inp.nextInt();
             }
         }
-        multiply(r1,c1,r2,c2,a,b);
+        multiply(r1, c1, r2, c2, a, b);
     }
 }
 
